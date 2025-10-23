4a1b0fe5b41586d3f18db78fb05424d886e09210
fmazur57u <florianmazur2@gmail.com>
2025-10-23 14:45:55 +0200

    Ajout du compteur

--- home.py
+++ home.py
@@ -1,7 +1,18 @@
 import streamlit as st
+from projet_flashcards import init_db, get_all_themes, get_number_of_cards
+import os
 
 st.set_page_config(
     page_title="Application de flashcard", page_icon=":house:", layout="wide"
 )
 
-st.title("Application de flashcard")
+if not os.path.exists("flashcards.db"):
+    init_db()
+
+all_theme = get_all_themes()
+
+if all_theme:
+    current_theme = all_theme[0]
+    st.sidebar.write(f"Nombre de carte total : {get_number_of_cards()}")
+else:
+    st.sidebar.write(f"Nombre de carte total : 0")
